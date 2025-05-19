9e0d538bdeed834d52a309827fa2f102aa6ea2f5
z-zeek <167027652+z-zeek@users.noreply.github.com>
2025-05-19 18:44:22 +0000

    add general conf

---
+++ configuration.nix
@@ -0,0 +1,145 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, inputs, lib, pkgs, ... }: 
+
+{
+  hardware.firmware = let
+    brcmFirmware = pkgs.runCommand "brcm-firmware" {
+      src = ./lib/firmware/brcm;
+    }  ''
+       mkdir -p $out/lib/firmware/brcm
+       cp -r $src/* $out/lib/firmware/brcm/
+    '';
+  
+  in [ brcmFirmware ];
+
+  # add env packages
+  environment.systemPackages = with pkgs; [
+    hyprland
+    wayland
+    wayland-utils
+    waybar
+    kitty
+    fish
+    wofi
+    rofi
+    brightnessctl
+    grim
+    slurp
+    wl-clipboard
+    xdg-desktop-portal-hyprland
+    networkmanager
+    networkmanagerapplet
+    iw
+    curl
+    wget
+    nixpkgs-fmt
+    nixfmt
+    dhcpcd
+    wpa_supplicant
+    vim
+    git
+    xdg-utils
+    dbus
+    pam
+    firefox
+    ags
+    anyrun
+    zsh
+    foot
+    fuzzel
+    libsForQt5.qt5ct
+    wlogout
+    zsh
+    nodejs_23
+    _1password-gui
+    _1password-cli
+    vscode
+    distrobox
+    podman
+  ];
+  
+  # enable hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  
+  # enable greetd
+  services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+          user = "zk";
+      };
+    };
+  };
+
+  serivces.waybar = {
+    enable = true;
+  };
+
+  services.xserver.enable = false;
+  services.dbus.enable = true;
+
+  hardware.opengl.enable = true;
+
+
+  # Portal support
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    xdgOpenUsePortal = true;
+  };
+
+  # enable pipewire (audio)
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    jack.enable = false;
+  };
+  
+  users.users.zk = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" "networkmanager" ];
+    packages = with pkgs; [
+      firefox
+      neovim
+    ];
+  };
+ 
+  # Enable networking and fonts
+  networking.hostName = "stockholm";
+  networking.networkmanager.enable = true;
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8"];
+    
+
+  fonts.packages = with pkgs; [ noto-fonts noto-fonts-emoji ];
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader= {
+    efi.efiSysMountPoint = "/mnt/boot";
+    systemd-boot.enable = true;
+  };
+ 
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Enable touchpad support (enabled default in most desktopManager).
+   services.libinput.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  system.stateVersion = "25.05";
+
+}
---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.soopy.moe"
+    ];
+    extra-trusted-public-keys = [ "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=" ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      # inputs.nixpkgs.follows = "hyprland";
+    };
+
+        ags.url = "github:Aylur/ags";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    gross = {
+      url = "github:fufexan/gross";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+    matugen = {
+      url = "github:/InioX/Matugen";
+      # ref = "refs/tags/matugen-v0.10.0"
+    };
+    more-waita = {
+      url = "github:somepaulo/MoreWaita";
+      flake = false;
+    };
+    firefox-gnome-theme = {
+      url = "github:rafaelmardojai/firefox-gnome-theme";
+      flake = false;
+    };
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs =
+    { nixpkgs, nixos-hardware, ... }:
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+
+      nixosConfigurations.stockholm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./nix/substituter.nix
+          nixos-hardware.nixosModules.apple-t2
+        ];
+      };
+    };
+}
